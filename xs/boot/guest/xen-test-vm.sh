17e27c0a023f4bd9aaf7f38a8aede59e1708fd77
Christian Lindig <christian.lindig@citrix.com>
2016-04-14 17:14:26 +0100

    CA-16626 Added check to avoid re-downloading xen-test VM
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- xs/boot/guest/xen-test-vm.sh
+++ xs/boot/guest/xen-test-vm.sh
@@ -3,14 +3,22 @@
 # xen-test-vm.sh [version] - downloads template of VM
 #
 
+FORCE="no"
 VERSION="${1:-0.0.5}"
 NAME="xen-test-vm-$VERSION"
 GH="https://github.com/xapi-project"
 VM="$GH/xen-test-vm/releases/download/$VERSION/test-vm.xen.gz"
+KERNEL="xen-test-vm-${VERSION//./-}.xen.gz"
 
 errcho() { echo "$@" 1>&2; }
 
-KERNEL="xen-test-vm-${VERSION//./-}.xen.gz"
+
+if test -f ${KERNEL}; then
+  errcho "Version ${VERSION} already present: ${KERNEL}"
+  errcho "Remove it first to download it again"
+  exit 0
+fi
+
 curl --fail -s -L "$VM" > "$KERNEL"
 if [ $? -ne 0 ]; then
   errcho "Can't download $VM"
