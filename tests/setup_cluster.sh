768e5e0c55e2bbe4398f632d6ccc790a18387637
Edwin Török <edvin.torok@citrix.com>
2017-07-06 17:42:41 +0100

    setup cluster on all N VMs
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- tests/setup_cluster.sh
+++ tests/setup_cluster.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
+set -x
 set -e
-vagrant up cluster{1,2,3} --no-provision
-vagrant provision cluster{1,2,3}
 
 # workaround NilClass exception
 vagrant up infrastructure || vagrant up infrastructure
 
+HOSTS=$(vagrant status |  grep ^cluster |cut -f1 -d' '|xargs echo)
+vagrant up $HOSTS
+echo $HOSTS
+
 echo "Cluster auth"
 echo "============"
-
-vagrant ssh cluster1 -c "sudo pcs cluster auth -u hacluster -p mysecurepassword cluster1 cluster2 cluster3"
-
+vagrant ssh cluster1 -c "sudo pcs cluster auth -u hacluster -p mysecurepassword $HOSTS"
 
 echo "Cluster setup"
 echo "============="
 
-vagrant ssh cluster1 -c "sudo pcs cluster setup --name cluster cluster1 cluster2 cluster3"
-
+vagrant ssh cluster1 -c "sudo pcs cluster setup --name cluster $HOSTS"
 
 echo "Cluster start"
 echo "============="
 
+vagrant ssh cluster1 -c "sudo pcs cluster enable --all"
 vagrant ssh cluster1 -c "sudo pcs cluster start --all"
-
-
