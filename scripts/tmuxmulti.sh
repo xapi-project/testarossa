ae7bbac4e75b3aa51553edb559aae59215550713
Edwin Török <edvin.torok@citrix.com>
2017-09-22 15:37:08 +0100

    Add 'make watch'
    When run from inside `tmux`, this will open up 4 panes showing information from
    each vagrant node.
    The watch is done outside, so that when the VM is down it will keep retrying.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

---
+++ scripts/tmuxmulti.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# You must run this script from inside tmux!
+set -e
+WINDOW="$*"
+CMD="$1"
+shift
+if [ $# = 0 ]; then
+    echo "Missing arguments" >&2
+    exit 1
+fi
+tmux set -g default-terminal screen
+echo $* | xargs -n1 | xargs -I {} echo "${CMD}" | (
+    read EXPANDEDCMD
+    tmux new-window -d -n "${WINDOW}" sh -c "set -x; $EXPANDEDCMD"
+    while read EXPANDEDCMD; do
+        tmux split-window -v -t "${WINDOW}" sh -c "set -x; $EXPANDEDCMD"
+        tmux select-layout -t "${WINDOW}" tiled
+    done
+)
